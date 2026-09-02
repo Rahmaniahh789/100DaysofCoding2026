c29bb46d5bc88a84d217ac44816b890908865161
Rahmaniahh789 <rahmaniahh789@gmail.com>
2026-09-02 23:38:18 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,18 @@
+public class Day1{
+// komentar satu baris yang berisi komentar singkat
+
+/* komentar dokumentasi beberapa baris
+ *disini kamu bisa menambahkan deskripsi panjang mengenai materi
+*/
+
+/** komentar dokumentasi resmi
+ *contoh
+ * @return pesan sambutan lengkap
+*/
+
+public static void main(String[] args) {
+
+  System.out.print("belajar ngoding");
+    }
+
+}
