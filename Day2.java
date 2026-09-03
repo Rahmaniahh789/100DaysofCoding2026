6849237125ab055ee9dc8d637dd72f919b663cea
Rahmaniahh789 <rahmaniahh789@gmail.com>
2026-09-03 22:17:51 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,32 @@
+public class Day02 {
+    public static void main(String[] args) {
+        String nama, alamat, hobi, nim;
+        int umur;
+
+        nama = "Rahmadiah";
+        alamat = "Pambusuang";
+        hobi = "Memasak";
+        umur = 18;
+        nim = "D0226313";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Hobi: " + hobi);
+        System.out.println("Umur: " + umur);
+        System.out.println("NIM: " + nim);
+        System.out.println("\n");
+
+        System.out.print("Nama: " + nama + "\n");
+        System.out.print("Alamat: " + alamat + "\n");
+        System.out.print("Hobi: " + hobi + "\n");
+        System.out.print("Umur: " + umur + "\n");
+        System.out.print("NIM: " + nim + "\n");
+        System.out.print("\n");
+
+        System.out.printf("Nama: %s%n", nama);
+        System.out.printf("Alamat: %s%n", alamat);
+        System.out.printf("Hobi: %s%n", hobi);
+        System.out.printf("Umur: %d%n", umur);
+        System.out.printf("NIM: %s%n", nim);
+    }
+          }
