85b07ad43ccfd637eae27e3416d2374010d1e4a2
Rahmaniahh789 <rahmaniahh789@gmail.com>
2026-09-04 22:37:02 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,21 @@
+public class day3 {
+    public static void main(String[] args) {
+        // \n : Membuat baris baru (newline)
+        System.out.println("Baris Pertama\nBaris Kedua");
+
+        // \t : Membuat tabulasi vertikal/jarak mendatar (tab)
+        System.out.println("Nama:\t RAHMANIAH");
+        System.out.println("NIM:\t D0229313");
+        System.out.println("Kelas:\t Informatika B");
+        System.out.println("Hobi:\t Memasak dan menari");
+
+        // \" : Menampilkan tanda kutip ganda
+        System.out.println("Dia berkata, \"Halo niahhhhh\"");
+
+        // \' : Menampilkan tanda kutip tunggal
+        System.out.println("Karakter: \'A\'");
+
+        // \r : Carriage return (mengembalikan kursor ke awal baris, menimpa teks sebelumnya)
+        System.out.println("Teks Lama\rTeks Baru");
+    }
+          }
