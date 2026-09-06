6c63003ae870e77969059f89fd5a4df6b0329b1b
Rahmaniahh789 <rahmaniahh789@gmail.com>
2026-09-06 22:13:30 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,14 @@
+public class day5 {
+    public static void main(String[] args) {
+      // float (32-bit): pakai akhiran 'f', untuk angka desimal standar
+        float tinggi = 155.5f;
+
+      // double (64-bit): default Java, lebih akurat untuk angka desimal
+        double ipk = 3.85;
+
+      //menampilkan
+
+        System.out.println("Tinggi niahh : " + tinggi + " cm");
+        System.out.println("IPK niahh    : " + ipk);
+    }
+}
