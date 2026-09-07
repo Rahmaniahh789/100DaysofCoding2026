fac6d28f2d199b6f006a495757d6ee9e0e984058
Rahmaniahh789 <rahmaniahh789@gmail.com>
2026-09-07 15:59:33 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,13 @@
+public class day6 {
+    public static void main(String[] args) {
+    // Tipe data char: menyimpan SATU karakter (harus pakai tanda petik tunggal ' ')
+        char nilaiHuruf = 'A';
+
+    // Tipe data boolean: hanya menyimpan nilai kebenaran (true atau false)
+        boolean isLulus = true;
+
+    // Menampilkan nilai variabel ke layar
+        System.out.println("Nilai Ujian: " + nilaiHuruf);
+        System.out.println("Status Lulus: " + isLulus);
+    }
+          }
