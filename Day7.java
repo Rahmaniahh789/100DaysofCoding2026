9379b4937f9fa378536956676cec33ac49f7001e
Rahmaniahh789 <rahmaniahh789@gmail.com>
2026-09-08 22:54:43 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,15 @@
+public class day7 {
+	public static void main(String[] args) {
+
+    String nama = "RAHMANIAH";
+    String prodi = "informatika";
+    String alamat = "BALA";
+    String kelas = "B";
+
+    System.out.println("nama    = " + nama);
+    System.out.println("prodi   = " + prodi);
+    System.out.println("alamat  = " + alamat);
+    System.out.println("kelas   = " + kelas);
+
+   }
+     }
