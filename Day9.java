cbacf2fa9610cc83ac713a4ac591f17986825edb
Rahmaniahh789 <rahmaniahh789@gmail.com>
2026-09-10 20:21:00 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,12 @@
+public class day9 {
+  public static void main(String[] args) {
+    // Deklarasi variabel final (konstanta): nilainya tidak bisa diubah setelah diisi
+    final String nama = "Rahmaniahh";
+    final String nim = "D0226***";
+    
+    // Mencetak nilai konstanta ke layar
+    System.out.println("Nama : " + nama);
+    System.out.println("Nim : " + nim);
+  
+  }
+}
