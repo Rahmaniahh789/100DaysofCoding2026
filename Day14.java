7afb1199d5a90cea25a78f1273b73772327b212e
Rahmaniahh789 <rahmaniahh789@gmail.com>
2026-09-15 22:30:43 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,18 @@
+public class  Day14{
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 7;
+
+        // Operator penjumlahan
+        int tambah = a + b;
+
+        // Operator pengurangan
+        int kurang = a - b;
+
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("--------------------");
+        System.out.println("Hasil penjumlahan a + b = " + tambah);
+        System.out.println("Hasil pengurangan a - b = " + kurang);
+    }
+                           }
