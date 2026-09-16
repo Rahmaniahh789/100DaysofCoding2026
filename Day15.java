cc88678fea2a35766a3d6b59a6b42cb322c314ce
Rahmaniahh789 <rahmaniahh789@gmail.com>
2026-09-16 22:12:05 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+public class Day15 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+      
+        int kali = a * b;
+        int bagi = a / b;
+      
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+      
+        System.out.println("Hasil perkalian a * b = " + kali);
+        System.out.println("Hasil pembagian a / b = " + bagi);
+    }
+          }
