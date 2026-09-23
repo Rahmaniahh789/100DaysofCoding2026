4e81f92ac4b3965fdaaf3c0bac32977f08d73e29
Rahmaniahh789 <rahmaniahh789@gmail.com>
2026-09-23 23:12:02 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class day22{
+
+public static void main(String[] args) {
+
+Scanner input = new Scanner(System.in);
+
+System.out.println(x: "masukkan angka : ");
+int a = input.nextInt();
+
+System.out.println(x: "masukkan angka ke dua: ");
+int b = input.nextInt();
+
+System.out.println("sebelum a di ubah : " + a);
+System.out.println("sebelum b di ubah : " + b);
+
+a=ab;
+b = a + a;
+
+System.out.println("setelah a di ubah: " + a);
+System.out.println("setelah b di ubah: " + b);
+
+}
+  }
